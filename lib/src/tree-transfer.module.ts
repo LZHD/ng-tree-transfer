c5b914c7ffb3eece17706c44f3b92204ef552f11
LZHD <lzhdfxq@163.com>
2019-05-03 13:09:09 +0800

    fix: import package error

--- lib/src/tree-transfer.module.ts
+++ lib/src/tree-transfer.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {TreeTransferComponent} from './tree-transfer.component';
-import {NgZorroAntdModule} from 'ng-zorro-antd';
 import {FormsModule} from '@angular/forms';
-import {StringTemplateOutletDirective} from 'ng-tree-transfer/src/string-template-outlet.directive';
+import {NgZorroAntdModule} from 'ng-zorro-antd';
+import {TreeTransferComponent} from './tree-transfer.component';
+import {StringTemplateOutletDirective} from './string-template-outlet.directive';
 
 @NgModule({
   declarations: [
