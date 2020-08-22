03d30de12a03063ddd7498fd68da16aa45504373
LZHD <lzhdfxq@163.com>
2020-08-22 21:40:48 +0800

    chore: update to angular9.0

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ng-tree-transfer!');
+    expect(page.getTitleText()).toEqual('ng-tree-transfer app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import {CommonModule, registerLocaleData} from '@angular/common';
-import {TreeTransferModule} from 'ng-tree-transfer/src/tree-transfer.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NgZorroAntdModule, NZ_I18N, zh_CN} from 'ng-zorro-antd';
-import zh from '@angular/common/locales/zh';
 
-registerLocaleData(zh);
+import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { TreeTransferModule } from 'ng-tree-transfer';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -17,10 +14,8 @@ registerLocaleData(zh);
     BrowserModule,
     BrowserAnimationsModule,
     CommonModule,
-    NgZorroAntdModule,
     TreeTransferModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
