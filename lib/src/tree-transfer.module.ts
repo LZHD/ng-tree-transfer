0d84df0cb09e274cef1aa5c870d04a533d292888
LZHD <lzhdfxq@163.com>
2021-02-05 12:47:25 +0800

    chore: update to angular10

--- lib/src/tree-transfer.module.ts
+++ lib/src/tree-transfer.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { NzIconModule, NzButtonModule, NzInputModule, NzCheckboxModule, NzTreeModule } from 'ng-zorro-antd';
+
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzTreeModule } from 'ng-zorro-antd/tree';
+
 import { TreeTransferComponent } from './tree-transfer.component';
 import { StringTemplateOutletDirective } from './string-template-outlet.directive';
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {NzTreeNodeOptions} from 'ng-zorro-antd';
+import {NzTreeNodeOptions} from 'ng-zorro-antd/tree';
 
 @Component({
   selector: 'app-root',
